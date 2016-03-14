5f693bb50f2c9a7469c0f3dd9000aadff9698307
asranet <info@asranet.co.uk>
2016-03-14 14:15:25 +0000

    Added a co-authors field in Submit Abstract and added conference name to emails sent to clients

--- uploadAbstract/models.py
+++ uploadAbstract/models.py
@@ -5,19 +5,22 @@ from django.db import models
 
 class SubmittedAbstract(models.Model):
     salutation = models.CharField(max_length=6)
-    first_name = models.CharField(max_length=128)
-    last_name = models.CharField(max_length=128)
+    primary_author_first_name = models.CharField(max_length=35)
+    primary_author_surname = models.CharField(max_length=35)
+    co_authors_names = models.CharField(max_length=770)
     email = models.EmailField(max_length=60, unique=True)
     paper_title = models.CharField(max_length=60)
     abstract = models.CharField(max_length=2000)
 
     def save(self, *args, **kwargs):
 
-        sorted_self = [["Salutation", self.salutation], ["First name", self.first_name], ["Last Name", self.last_name],
-                       ["Email", self.email], ["Paper Title", self.paper_title], ["Abstract", self.abstract],
+        sorted_self = [["Salutation", self.salutation], ["Primary Author First Name", self.primary_author_first_name],
+                       ["Primary Author Surname", self.primary_author_surname],
+                       ["Co Authors Names", self.co_authors_names], ["Email", self.email],
+                       ["Paper Title", self.paper_title], ["Abstract", self.abstract],
                        ]
 
-        email_client(self, "NUPP 2017 Abstract Upload", "You have uploaded an abstract.")
+        email_client(self, "NUPP 2017 Abstract Upload", "Thank you for uploading an abstract for NUPP 2017.")
         email_admin(self, "New NUPP 2017 Abstract", "Please find enclosed the details for the new NUPP "
                                                     "2017 abstract upload.", sorted_self)
         super(SubmittedAbstract, self).save(*args, **kwargs)
